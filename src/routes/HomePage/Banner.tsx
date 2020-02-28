5f56f95b928eb8646f83b94749df724ec0d848e7
jsonjosn <jsonjosnflb@gmail.com>
2020-02-28 21:40:07 +0800

    解决插件样式引入问题

--- src/routes/HomePage/Banner.tsx
+++ src/routes/HomePage/Banner.tsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import { helper } from '@/utils';
-import style from './style.scss';
+import React, { useState, useEffect, useRef } from "react";
+import Slider from "react-slick";
+import Typed from "typed.js";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { helper } from "@/utils";
+import style from "./style.scss";
 
 // 参数
 const settings: any = {
@@ -20,22 +21,29 @@ const settings: any = {
 // banner数据
 const datas = [
   {
-    src: './statics/images/home/slider1.jpg'
+    src: "./statics/images/home/slider1.jpg"
   },
   {
-    src: './statics/images/home/slider2.jpg'
+    src: "./statics/images/home/slider2.jpg"
   },
   {
-    src: './statics/images/home/slider3.jpg'
+    src: "./statics/images/home/slider3.jpg"
   },
   {
-    src: './statics/images/home/slider4.jpg'
+    src: "./statics/images/home/slider4.jpg"
   }
 ];
 
 const Banner = () => {
+  useEffect(() => {
+    var typed = new Typed("#typed", {
+      stringsElement: "#typed-strings",
+      typeSpeed: 40
+    });
+  }, []);
+
   return (
-    <div className={helper.concatClass(style.banner_wrap, 'home_banner_wrap')}>
+    <div className={helper.concatClass(style.banner_wrap, "home_banner_wrap")}>
       <Slider {...settings}>
         {datas.map((item, index) => (
           <div key={index} className={style.slick_item}>
@@ -47,7 +55,15 @@ const Banner = () => {
         ))}
       </Slider>
       <div className={style.content_wrap}>
-        <h1>{'title'}</h1>
+        <div id={"typed-strings"}>
+          <p>
+            Typed.js is a <strong>JavaScript</strong> library.
+          </p>
+          <p>
+            It <em>types</em> out sentences.
+          </p>
+        </div>
+        <span id={"typed"}></span>
       </div>
     </div>
   );
--- src/routes/HomePage/HomePage.tsx
+++ src/routes/HomePage/HomePage.tsx
@@ -14,7 +14,7 @@ const HomePage = (props: any) => {
   }, []);
 
   return (
-    <div>
+    <div className={style.HomePage}>
       <Banner />
     </div>
   );
